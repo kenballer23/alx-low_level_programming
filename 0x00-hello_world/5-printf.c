d8261a24ec230d8e616ee106d8c7342d35aecc0a
kenballer23 <ken10asare@gmail.com>
2023-03-16 11:58:59 -0700

    stuff.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - prints a line of code using printf
  *
- * return: 0
+ * Return: 0
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,"\n);
-	return(0);
-}	
+	return (0);
+}
