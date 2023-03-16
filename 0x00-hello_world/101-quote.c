317b77cd9fb2480546175c2b262fa7f8a19702a4
kenballer23 <ken10asare@gmail.com>
2023-03-16 15:21:46 -0700

    stuff.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - primt without using puts or printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
