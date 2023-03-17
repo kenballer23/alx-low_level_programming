ad56ddf2892a3202a258c2c177ec3a01834b8df0
kenballer23 <ken10asare@gmail.com>
2023-03-17 05:15:21 -0700

    VAR

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+
+
+}
