68077104c5a632ddce115eb1efea10a63cface92
kenballer23 <ken10asare@gmail.com>
2023-03-17 19:54:40 -0700

    VAR

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 98 ; i++)
+	{
+		for (j = i + 1 ; j <= 99 ; j++)
+		{
+			int n1 = i / 10;
+			int n2 = i % 10;
+			int n3 = j / 10;
+			int n4 = j % 10;
+
+			putchar(n1 + '0');
+			putchar(n2 + '0');
+			putchar(',');
+			putchar(n3 + '0');
+			putchar(n4 + '0');
+			if (i == 98 && j == 99)
+			continue;
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
