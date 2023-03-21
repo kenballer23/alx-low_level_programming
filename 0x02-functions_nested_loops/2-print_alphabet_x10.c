be38b8debd0f84f239fd41e2eaa5d2f49c30d801
kenballer23 <ken10asare@gmail.com>
2023-03-21 11:29:33 -0700

    0X02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j = 0;
+
+	while (j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+
+}
