87a854f3cea1a493bae12a71241f5d246af380e1
kenballer23 <ken10asare@gmail.com>
2023-03-28 11:43:21 -0700

    your point?

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: destination array
+ * @src: source array
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
